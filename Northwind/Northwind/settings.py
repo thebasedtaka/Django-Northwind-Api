5b44e646745bab445c1eab70b03c2e3edefed0aa
thebasedtaka <darktaka9998@gmail.com>
2025-05-26 22:16:48 -0400

    Cors

--- Northwind/Northwind/settings.py
+++ Northwind/Northwind/settings.py
@@ -187,6 +187,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "https://white-rock-0ecf1790f.6.azurestaticapps.net"
 ]
 
 DASHBOARD_CACHE_TIMEOUT = 300
